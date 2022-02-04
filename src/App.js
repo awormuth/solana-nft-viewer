034325492309079a3d532bcfa36f794ddcc3851e
DESKTOP-4HOL7H1\kbikh <kbikhazi@hotmail.com>
2022-02-03 22:31:17 -0800

    integrated playcanvas iframe

--- src/App.js
+++ src/App.js
@@ -180,41 +180,6 @@ const App = () => {
     if (nftList.length > 0) {
       console.log("Loaded NFTs:");
       console.log(nftList);
-
-      //const app = new pc.Application(document.getElementById('game'));
-      //app.setCanvasResolution(pc.RESOLUTION_AUTO);
-
-      //const app = new pc.Application(canvas, {});
-      const app = new pc.Application(document.getElementById("game"));
-      app.setCanvasResolution(pc.RESOLUTION_AUTO);
-
-      // create box entity
-      const box = new pc.Entity("cube");
-      box.addComponent("render", {
-        type: "box",
-      });
-
-      app.root.addChild(box);
-
-      // create camera entity
-      const camera = new pc.Entity("camera");
-      camera.addComponent("camera", {
-        clearColor: new pc.Color(0.5, 0.6, 0.9),
-      });
-
-      app.root.addChild(camera);
-      camera.setPosition(0, 0, 3);
-
-      // create directional light entity
-      const light = new pc.Entity("light");
-      light.addComponent("light");
-      app.root.addChild(light);
-      light.setEulerAngles(45, 0, 0);
-
-      // rotate the box according to the delta time since the last frame
-      app.on("update", (dt) => box.rotate(10 * dt, 20 * dt, 30 * dt));
-
-      app.start();
     }
   }, [nftList]);
 
@@ -230,7 +195,7 @@ const App = () => {
           {/* Add the condition to show this only if we don't have a wallet address */}
           {!walletAddress && renderNotConnectedContainer()}
           {walletAddress && renderConnectedContainer()}
-          <canvas id="game" className="play-canvas-style"></canvas>
+          <iframe src="http://localhost:8080/" title="PlayCanvas"></iframe>
         </div>
         <div className="footer-container"></div>
       </div>
